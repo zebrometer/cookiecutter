fd3c612762fda235dbfd5667b201887d82ba3be9
Igor Sergeev <igor.sergeev@stratacloud.com>
2016-05-19 16:53:54 -0400

    try packing

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // https://github.com/SheetJS/js-xlsx
-//https://github.com/jakesgordon/bin-packing
+// https://github.com/jakesgordon/bin-packing
 
 var bootstrapApp =  ((function() {
 	var ALPHABET         = 'abcdefghijklmnopqrstuvwxyz'
@@ -14,14 +14,14 @@ var bootstrapApp =  ((function() {
 		quantity      : letterAt(13)
 	}
 
-	var pdfs = []	
+	var pdfs = []
 
-	function initEvent() {		
+	function initEvent() {
 		$('.dropTarget').on('drop dragdrop', function(event) {
 	    event.preventDefault()
-	    
-	    var file = event.originalEvent.dataTransfer.files 
-	    	&& event.originalEvent.dataTransfer.files.length > 0 
+
+	    var file = event.originalEvent.dataTransfer.files
+	    	&& event.originalEvent.dataTransfer.files.length > 0
 	    	&& event.originalEvent.dataTransfer.files[0]
 
 	    if (file) {
@@ -40,10 +40,10 @@ var bootstrapApp =  ((function() {
 		    event.preventDefault()
 		})
 
-		$('.export').on('click', function() {					
-			pdfs && pdfs.forEach((doc) => {						
+		$('.export').on('click', function() {
+			pdfs && pdfs.forEach((doc) => {
 				doc.save(doc.filename)
-			})					
+			})
 		})
 
 		$('.reset').on('click', function() {
@@ -55,13 +55,13 @@ var bootstrapApp =  ((function() {
 	function parseFile(file) {
 		var reader = new FileReader()
 
-		reader.onload = function(e) {					
+		reader.onload = function(e) {
 			var data = e.target.result
 			var workbook = XLSX.read(data, {type: 'binary'})
-								
-			workbook.SheetNames 
-				&& workbook.SheetNames.length > 0 
-				&& processWorksheet(workbook.Sheets[workbook.SheetNames[0]])										
+
+			workbook.SheetNames
+				&& workbook.SheetNames.length > 0
+				&& processWorksheet(workbook.Sheets[workbook.SheetNames[0]])
 		}
 
 		reader.readAsBinaryString(file)
@@ -98,13 +98,13 @@ var bootstrapApp =  ((function() {
 		}
 
 		return {
-				start: startIndex, 
+				start: startIndex,
 				end: endIndex
 			}
 	}
 
 	function letterAt(index) {
-		return index < (ALPHABET.length - 1) 
+		return index < (ALPHABET.length - 1)
 			&& ALPHABET[index].toUpperCase()
 	}
 
@@ -127,16 +127,16 @@ var bootstrapApp =  ((function() {
 
 				if (cellValue && cellValue.v) {
 					value[prop] = cellValue.v
-				}						
-			}					
-		})
-									
-		var hasMissingValues = Object.keys(value).some(function(prop) {  
-			return typeof value[prop] === 'undefined' 
+				}
+			}
 		})
 
-		return hasMissingValues 
-			? void 0 
+		var hasMissingValues = Object.keys(value).some(function(prop) {
+			return typeof value[prop] === 'undefined'
+		})
+
+		return hasMissingValues
+			? void 0
 			: value
 	}
 
@@ -144,7 +144,7 @@ var bootstrapApp =  ((function() {
 		var values = []
 
 		if (range.start >= 0 && range.end >= 0) {
-			for (var i=range.start; i<=range.end; i++) {						
+			for (var i=range.start; i<=range.end; i++) {
 				var value = findValue(i, worksheet)
 				value && values.push(value)
 			}
@@ -153,9 +153,9 @@ var bootstrapApp =  ((function() {
 		return values
 	}
 
-	function processWorksheet(worksheet) {						
+	function processWorksheet(worksheet) {
 		var range  = findRange(worksheet)
-		var values = findValues(range, worksheet)				
+		var values = findValues(range, worksheet)
 
 		// var range    = worksheet['!range']
 		// var columns  = (range.e && !isNaN(range.e.c) && +range.e.c) || 0
@@ -173,12 +173,53 @@ var bootstrapApp =  ((function() {
 	}
 
 	function initData(data) {
-		var nSteps   = data && data.length
-		var deferred = data && data.map((function(datum, index) { 
-			return addPdfFrame.bind({datum: datum, index: index, nSteps: nSteps}) 
-		}))
+		// var nSteps   = data && data.length
+		// var deferred = data && data.map((function(datum, index) {
+		// 	return addPdfFrame.bind({datum: datum, index: index, nSteps: nSteps})
+		// }))
+		//
+		// nextDeferred(deferred)
+		data = data || []
 
-		nextDeferred(deferred)
+		var blocks = data.map(function(datum) {
+			return {
+				w: datum.width,
+				h: datum.height,
+				o: datum
+			}
+		}).sort(function(o1, o2) {
+			return o2.h - o1.h
+		})
+
+		var orderedBlocks = blocks.slice()
+
+		var documentWidth  = 50
+		var documentHeight = 50
+
+		var packer = new Packer(documentWidth, documentHeight)
+ 		packer.fit(orderedBlocks)
+		createPackerDocument(orderedBlocks, documentWidth, documentHeight)
+	}
+
+	function createPackerDocument(blocks, documentWidth, documentHeight) {
+		var doc = new jsPDF('p', 'in', [documentWidth, documentHeight])
+		doc.setLineWidth(1/72)
+
+		blocks.forEach(function(block, index) {
+			if (block.fit) {
+				var color = Math.floor(Math.random() * 150)
+
+				doc.setFillColor(color, color, color)
+				doc.rect(block.fit.x, block.fit.y, block.w, block.h, 'F')
+
+				doc.setTextColor(255, 255, 255)
+				var hintMessage = block.o.width + ' x ' + block.o.height + '  Color: ' + block.o.color + " -- N:" + (index + 1)
+				doc.text(block.fit.x + .1, block.fit.y + .3, hintMessage, 0)
+			} else {
+				console.log("No fit!")
+			}
+		})
+		doc.save()
 	}
 
 	function nextDeferred(deferred) {
@@ -186,17 +227,17 @@ var bootstrapApp =  ((function() {
 		nextDeferred && nextDeferred(deferred)
 	}
 
-	function addPdfFrame(deferred) {		
+	function addPdfFrame(deferred) {
 		var datum  = this.datum
 		var index  = this.index
-		var nSteps = this.nSteps		
+		var nSteps = this.nSteps
 
 		setTimeout(function() {
 			$('.container').append("<iframe class='preview-pane' type='application/pdf' style='width: 200%; height: 200%;' frameborder='0' src=" + generatePDF(datum) + "></iframe>")
 
 			var percentComplete = Math.floor(100 * index/ nSteps)
 
-			if (deferred.length === 0) {				
+			if (deferred.length === 0) {
 				$('.indicator').css('left', 0)
 				$('.indicator').css('width', 0)
 				$('.progress > .indicator > span').text('')
@@ -204,17 +245,17 @@ var bootstrapApp =  ((function() {
 				$('.indicator').css('left', '20px')
 				$('.indicator').css('width', percentComplete+'%')
 				$('.progress > .indicator > span').text(percentComplete+'%')
-			}			
+			}
 
 			nextDeferred(deferred)
-		}, 500)			
+		}, 500)
 	}
 
 	/**
 	 * Units: 'pt', 'mm', 'cm', 'in', 'px', 'pc', 'em', 'ex'
-	 * http://mrrio.github.io/jsPDF/doc/symbols/jsPDF.html			 
+	 * http://mrrio.github.io/jsPDF/doc/symbols/jsPDF.html
 	 */
-	function generatePDF(datum) {				
+	function generatePDF(datum) {
 		var swap  = datum.width > datum.height
 
 		var width         = swap ? datum.height        : datum.width
@@ -223,18 +264,18 @@ var bootstrapApp =  ((function() {
 		var paddingRight  = swap ? datum.paddingBottom : datum.paddingRight
 		var paddingTop    = swap ? datum.paddingLeft   : datum.paddingTop
 		var paddingBottom = swap ? datum.paddingRight  : datum.paddingBottom
-		
+
 		var totalWidth    = paddingLeft + width  + paddingRight
 		var totalHeight   = paddingTop  + height + paddingBottom
 
 		var doc      = new jsPDF('p', 'in', [totalWidth, totalHeight])
 		doc.tag_key  = datum.width
-
 		doc.filename = clean(datum.width) + 'x' + clean(datum.height) + '_' + datum.color
+
 
 		doc.setDrawColor(100, 100, 100)
 		doc.setLineWidth(1/72)
-		doc.rect(paddingLeft, paddingTop, width, height, 'S')
+		doc.rect(x + paddingLeft, y + paddingTop, width, height, 'S')
 
 		var hintMessage = datum.width + ' x ' + datum.height + ' (' + totalWidth + ' x ' + totalHeight + ')   Color: ' + datum.color
 		doc.text(.1, totalHeight - .1, hintMessage, 0)
@@ -243,7 +284,32 @@ var bootstrapApp =  ((function() {
 		return doc.output('datauristring')
 	}
 
+	function populatePDF(datum, x, y, doc) {
+		x = x || 0
+		y = y || 0
+
+		//var swap  = datum.width > datum.height
+		var swap  = false
+
+		var width         = swap ? datum.height        : datum.width
+		var height        = swap ? datum.width         : datum.height
+		var paddingLeft   = swap ? datum.paddingTop    : datum.paddingLeft
+		var paddingRight  = swap ? datum.paddingBottom : datum.paddingRight
+		var paddingTop    = swap ? datum.paddingLeft   : datum.paddingTop
+		var paddingBottom = swap ? datum.paddingRight  : datum.paddingBottom
+
+		var totalWidth    = paddingLeft + width  + paddingRight
+		var totalHeight   = paddingTop  + height + paddingBottom
+
+		doc.setDrawColor(100, 100, 100)
+		doc.setLineWidth(1/72)
+		doc.rect(x + paddingLeft, y + paddingTop, width, height, 'S')
+
+		var hintMessage = datum.width + ' x ' + datum.height + ' (' + totalWidth + ' x ' + totalHeight + ')   Color: ' + datum.color
+		doc.text(x + .1, y + totalHeight - .1, hintMessage, 0)
+	}
+
 	return function bootstrapApp() {
 		initEvent()
-	}	
+	}
 }))()
